bae2d5a99d952ff61243e9c9e4c768c59b31c80d
krl82610 <krl82610@hawkmail.hacc.edu>
2024-10-22 21:29:45 -0400

    meeting update

--- minecraftgazebo/script.js
+++ minecraftgazebo/script.js
@@ -1,14 +1,14 @@
-const hamburger = document.querySelector(".hamburger");
-const navbara = document.querySelector(".navbar a");
+const hamburger = document.querySelector("#hamburger");
+const list = document.querySelector("#list");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
-  navbara.classList.toggle("active");
+  list.classList.toggle("active");
 });
 
 document.querySelectorAll(".nav-link").forEach((n) =>
   n.addEventListener("click", () => {
     hamburger.classList.remove("active");
-    navbara.classList.remove("active");
+    list.classList.remove("active");
   })
 );
